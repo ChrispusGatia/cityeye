e5715e12e2569a0020ed92bb7d225d39ab0ec720
Nibezak <kevin.nibeza@gmail.com>
2024-04-24 17:30:26 +0300

    added services , newsletter , and career

---
+++ content/collections/newsletter/newsletters.md
@@ -0,0 +1,8 @@
+---
+id: 538b00aa-b990-416b-b075-c07d357466d5
+blueprint: newsletter
+title: Newsletters
+author: 14d3b439-c1ca-4786-8ce7-59caffb630ff
+updated_by: 14d3b439-c1ca-4786-8ce7-59caffb630ff
+updated_at: 1713964842
+---
---
+++ resources/views/newsletter/show.blade.php
@@ -0,0 +1,95 @@
+<x-layout>
+
+    <head>
+        <!-- Basic Page Needs ================================================== -->
+        <meta charset="utf-8" />
+        <title>City Eye Hospital - NewsLetter</title>
+
+        <!-- Mobile Specific Metas ================================================== -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="City Eye Hospital - News and Events" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+        <meta name="author" content="City Eye Hospital" />
+        <meta name="generator" content="City Eye Hospital News and Events Page v1.0" />
+
+        <!-- Theme Meta ================================================== -->
+        <meta name="theme-name" content="CityEye" />
+
+        <!-- Favicon ================================================== -->
+        <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
+
+        <!-- Essential Stylesheets ================================================== -->
+        <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css" />
+        <link rel="stylesheet" href="plugins/icofont/icofont.min.css" />
+        <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css" />
+        <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css" />
+
+        <!-- Main Stylesheet ================================================== -->
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+
+    <body id="top">
+        <!-- Page Title Section -->
+        <section class="page-title bg-1">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="block text-center">
+                            <span class="text-white">NewsLetters</span>
+                            <h1 class="text-capitalize mb-5 text-lg">
+                                {{ $title }}
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Page Title Section -->
+
+        <!-- News Section -->
+        <section class="section blog-wrap">
+            <div class="container">
+                <div class="row">
+                    <!-- News Articles -->
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <!-- News Item 1 -->
+                            @foreach ($content as $item)
+                                <div class="col-lg-4 col-md-6 mb-4">
+                                    <div class="card">
+                                        <img src="images/news/general.jpg" class="card-img-top" alt="News Image" />
+                                        <div class="card-body">
+                                            <h5 class="card-title">
+                                                {{ $item->title }}
+                                            </h5>
+                                            <p class="card-text">
+                                                ${{ $item->description }}
+                                            </p>
+                                            <a href="generate-pdf.php?article_id=1" class="btn btn-primary"
+                                                target="{{ $item->link }}">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                            <!-- Add more news items as needed -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End News Section -->
+
+        <!-- Essential Scripts ================================================== -->
+        <script src="plugins/jquery/jquery.js"></script>
+        <script src="plugins/bootstrap/bootstrap.min.js"></script>
+        <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+        <script src="plugins/shuffle/shuffle.min.js"></script>
+
+        <!-- Google Map -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+        <script src="plugins/google-map/gmap.js"></script>
+
+        <script src="js/script.js"></script>
+    </body>
+</x-layout>

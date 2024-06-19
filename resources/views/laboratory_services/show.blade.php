ac5518b68b22dd3e2607c87faccc62fcadf23f99
chris <chrisdeveloperr@gmail.com>
2024-06-19 16:17:29 +0300

    changes

--- resources/views/laboratory_services/show.blade.php
+++ resources/views/laboratory_services/show.blade.php
@@ -3,7 +3,7 @@
     <head>
         <!-- Basic Page Needs ================================================== -->
         <meta charset="utf-8" />
-        <title>CityEye - Careers</title>
+        <title>City Eye Hospital - laboratory</title>
 
         <!-- Mobile Specific Metas ================================================== -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
